e5ca015a1a282c949d5af9fe47049c9f26f69353
Arno Lepisk <arno@lepisk.se>
2017-02-14 18:14:24 +0100

    First actual commit.

---
+++ Formula/gccexp.rb
@@ -0,0 +1,175 @@
+class GccExp < Formula
+  def arch
+    if MacOS.prefer_64_bit?
+      "x86_64"
+    else
+      "i686"
+    end
+  end
+
+  def osmajor
+    `uname -r`.chomp
+  end
+
+  desc "GNU compiler collection - experimental"
+  homepage "https://gcc.gnu.org"
+  revision 1
+
+  head "svn://gcc.gnu.org/svn/gcc/trunk"
+
+  # snapshots don't seem to be on the official mirrors, using direct link to a mirror
+  stable do
+    url "http://nl.mirror.babylon.network/gcc/snapshots/7-20170212/gcc-7-20170212.tar.bz2"
+    mirror "http://nl.mirror.babylon.network/gcc/snapshots/7-20170212/gcc-7-20170212.tar.bz2"
+    sha512 "3f8d7fdb5bcc82f57a3d4d8ee037bc421d25699abad7eb0e76b8a12f4fe31bf89c17a790a99cc95113374d227faf4a64a987762bddd4e0eb65739d90ca5f5ed4"
+  end
+
+  #remove a language options
+  option "with-nls", "Build with native language support (localization)"
+
+  # enabling multilib on a host that can't run 64-bit results in build failures
+  option "without-multilib", "Build without multilib support" if MacOS.prefer_64_bit?
+
+  depends_on "gmp"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "isl"
+  #depends_on "ecj" if build.with?("java") || build.with?("all-languages")
+
+  fails_with :gcc_4_0
+
+  # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
+  cxxstdlib_check :skip
+
+  # remove bottle stuff
+
+  # remove jit patch
+
+  # add conflict with official gcc 
+  conflicts_with "gcc@7", :because => "Experimental gcc and official gcc conflict"
+
+  def install
+    # GCC will suffer build errors if forced to use a particular linker.
+    ENV.delete "LD"
+
+    # only build c/c++
+    languages = %w[c c++]
+
+    # Even when suffixes are appended, the info pages conflict when
+    # install-info is run so pretend we have an outdated makeinfo
+    # to prevent their build.
+    ENV["gcc_cv_prog_makeinfo_modern"] = "no"
+
+    args = [
+      "--build=#{arch}-apple-darwin#{osmajor}",
+      "--prefix=#{prefix}",
+      "--libdir=#{lib}/gcc/#{version_suffix}",
+      "--enable-languages=#{languages.join(",")}",
+      # Make most executables versioned to avoid conflicts.
+      "--program-suffix=-#{version_suffix}",
+      "--with-gmp=#{Formula["gmp"].opt_prefix}",
+      "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+      "--with-mpc=#{Formula["libmpc"].opt_prefix}",
+      "--with-isl=#{Formula["isl"].opt_prefix}",
+      "--with-system-zlib",
+      "--enable-stage1-checking",
+      "--enable-checking=release",
+      "--enable-lto",
+      # Use 'bootstrap-debug' build configuration to force stripping of object
+      # files prior to comparison during bootstrap (broken by Xcode 6.3).
+      "--with-build-config=bootstrap-debug",
+      "--disable-werror",
+      "--with-pkgversion=Homebrew GCC #{pkg_version} #{build.used_options*" "}".strip,
+      "--with-bugurl=https://github.com/Homebrew/homebrew-core/issues",
+    ]
+
+    # The pre-Mavericks toolchain requires the older DWARF-2 debugging data
+    # format to avoid failure during the stage 3 comparison of object files.
+    # See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=45248
+    args << "--with-dwarf2" if MacOS.version <= :mountain_lion
+
+    args << "--disable-nls" if build.without? "nls"
+
+    #if build.with?("java") || build.with?("all-languages")
+    #  args << "--with-ecj-jar=#{Formula["ecj"].opt_share}/java/ecj.jar"
+    #end
+
+    if build.without?("multilib") || !MacOS.prefer_64_bit?
+      args << "--disable-multilib"
+    else
+      args << "--enable-multilib"
+    end
+
+    #args << "--enable-host-shared" if build.with?("jit") || build.with?("all-languages")
+
+    # Ensure correct install names when linking against libgcc_s;
+    # see discussion in https://github.com/Homebrew/homebrew/pull/34303
+    inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", "#{HOMEBREW_PREFIX}/lib/gcc/#{version_suffix}"
+
+    mkdir "build" do
+      unless MacOS::CLT.installed?
+        # For Xcode-only systems, we need to tell the sysroot path.
+        # "native-system-headers" will be appended
+        args << "--with-native-system-header-dir=/usr/include"
+        args << "--with-sysroot=#{MacOS.sdk_path}"
+      end
+
+      system "../configure", *args
+      system "make", "bootstrap"
+      system "make", "install"
+
+      #if build.with?("fortran") || build.with?("all-languages")
+      #  bin.install_symlink bin/"gfortran-#{version_suffix}" => "gfortran"
+      #end
+    end
+
+    # Handle conflicts between GCC formulae and avoid interfering
+    # with system compilers.
+    # Rename man7.
+    Dir.glob(man7/"*.7") { |file| add_suffix file, version_suffix }
+    # Even when we disable building info pages some are still installed.
+    info.rmtree
+  end
+
+  def add_suffix(file, suffix)
+    dir = File.dirname(file)
+    ext = File.extname(file)
+    base = File.basename(file, ext)
+    File.rename file, "#{dir}/#{base}-#{suffix}#{ext}"
+  end
+
+  def caveats
+    if build.with?("multilib") then <<-EOS.undent
+      GCC has been built with multilib support. Notably, OpenMP may not work:
+        https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60670
+      If you need OpenMP support you may want to
+        brew reinstall gcc --without-multilib
+      EOS
+    end
+  end
+
+  test do
+    (testpath/"hello-c.c").write <<-EOS.undent
+      #include <stdio.h>
+      int main()
+      {
+        puts("Hello, world!");
+        return 0;
+      }
+    EOS
+    system "#{bin}/gcc-#{version_suffix}", "-o", "hello-c", "hello-c.c"
+    assert_equal "Hello, world!\n", `./hello-c`
+
+    (testpath/"hello-cc.cc").write <<-EOS.undent
+      #include <iostream>
+      int main()
+      {
+        std::cout << "Hello, world!" << std::endl;
+        return 0;
+      }
+    EOS
+    system "#{bin}/g++-#{version_suffix}", "-o", "hello-cc", "hello-cc.cc"
+    assert_equal "Hello, world!\n", `./hello-cc`
+
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # homebrew-gccexp
 Test homebrew repo for experimental gcc.
+
+Based on gcc.rb from official homebrew-core
+
+Not for production use.
+
+
